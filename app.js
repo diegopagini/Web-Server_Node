bf63dfb9e0f594940626d59565d8dd746d317f22
Diego Pagini <diego.pagini@neoris.com>
2022-09-01 11:21:26 +0200

    static web served

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
 /** @format */
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const port = 8080;
+
+// instance of express.
 const app = express();
 
+// To serve static content:
+app.use(express.static('public')); // To use the content inside our public folder. This will be rendered in "/" path.
+
 // Routes
-app.get('/', (req, res) => {
-	res.send('Home page');
+app.get('/index', (req, res) => {
+	res.sendFile(path.join(__dirname, '/public', 'index.html')); // To show our index page.
 });
 
-app.get('/hello-world', (req, res) => {
-	res.send('Hello World from /hello-world route');
+app.get('/generic', (req, res) => {
+	res.sendFile(path.join(__dirname, '/public', 'generic.html')); // To show our generic page.
 });
 
-app.listen(8080);
+app.get('/elements', (req, res) => {
+	res.sendFile(path.join(__dirname, '/public', 'elements.html')); // To show our elements page.
+});
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, '/public', '404.html')); // To show our 404 page.
+});
+
+app.listen(port);
